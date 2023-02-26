bdc483842ac89f6bbbc9ad954a62a61ff881395b
vad1m4 <vad.levitskiy@gmail.com>
2023-02-26 15:22:11 +0200

    debug

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function print(text) {
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+window.addEventListener('scroll', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
