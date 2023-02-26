367e0676fa14c4e497f2ed42cde09d7f7cef2473
vad1m4 <vad.levitskiy@gmail.com>
2023-02-26 13:46:34 +0200

    projects + about me

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav_menu');
-const blob = document.getElementById("blob");
-const blur = document.getElementById("blur");
+function print(text) {
+    console.log(text)
+}
 
+// adds delay, i needed that to make the resize_blur work. if not for the delay, it would simply not work
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
+// moves the blurred blob in the background
+const blob = document.getElementById("blob");
 
-function move_blob(event) {
+function move_blob(event, speed) {
     const { clientX, clientY } = event;
 
     blob.animate({
         left: clientX + 'px',
         top: clientY + 'px'
-    }, { duration: 3000, fill: "forwards" });
+    }, { duration: speed, fill: "forwards" });
 }
+// added onclick for mobile devices
+document.body.onclick = event => {
+    move_blob(event, 1000)
+}
+
+document.body.onpointermove = event => {
+    move_blob(event, 3000)
+}
+// needed this because the blur layer stayed the same size as the window size when the site was loaded, exposing the raw blob when the window resized again
+const blur = document.getElementById("blur");
 
 function resize_blur(event) {
     blur.style.backdropFilter = `blur(79px)`;
     delay(10).then(() => {
-        console.log("resized");
         blur.style.backdropFilter = `blur(80px)`;
     })
 
@@ -29,28 +40,9 @@ function resize_blur(event) {
 window.onresize = event => {
     resize_blur(event);
 }
-
-
-// document.body.onpointermove = event => {
-//     const { clientX, clientY } = event;
-
-//     blob.animate({
-//         left: clientX + 'px',
-//         top: clientY + 'px'
-//     }, { duration: 3000, fill: "forwards"});
-
-// }
-
-
-
-document.body.onscroll = event => {
-    move_blob(event)
-}
-
-document.body.onpointermove = event => {
-    move_blob(event)
-}
-
+// stuff for hamburger menu (yes it's called a hamburger menu fuck off)
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_menu');
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -61,4 +53,93 @@ document.querySelectorAll(".nav_link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// changes blob's color when hovered over a clickable object
+const a = document.querySelector("a")
+
+document.querySelectorAll("a").forEach(n => n.addEventListener("mouseover", () => {
+    blob.classList.toggle("active");
+}, false))
+document.querySelectorAll("a").forEach(n => n.addEventListener("mouseout", () => {
+    blob.classList.remove("active");
+}, false))
+
+// observer for on-scroll animation of projects text
+var returnArr = Array
+const observed = new Event("Observed")
+const observer = new IntersectionObserver((entries) => {
+    returnArr = { key: Boolean };
+    entries.forEach((entry) => {
+        const target = entry.target.classList.value;
+        if (entry.isIntersecting) {
+            entry.target.classList.toggle("visible");
+            returnArr[target] = true;
+            dispatchEvent(observed)
+        } else {
+            entry.target.classList.remove("visible");
+            returnArr[target] = false;
+            dispatchEvent(observed)
+        }
+    });
+    return returnArr
+});
+
+
+const projects_text = document.querySelector(".projects_text")
+
+function anim_projects() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const target = document.querySelector(".projects_text")
+    let iterations = 0;
+    const interval = setInterval(() => {
+        target.innerText = target.innerText.split("")
+            .map((letter, index) => {
+                if (index == 2) {
+                    return " "
+                }
+                if (index < iterations - 1) {
+                    return target.dataset.value[index]
+                }
+                return letters[Math.round((Math.random() * 25))]
+            })
+            .join("")
+
+        if (iterations >= target.dataset.value.length) clearInterval(interval)
+
+        iterations += 1 / 3
+    }, 30)
+}
+
+observer.observe(projects_text)
+addEventListener(
+    "Observed",
+    (e) => {
+        if (returnArr["projects_text"]) {
+            anim_projects()
+        } else { };
+    },
+    false
+);
+document.querySelector(".projects_text").onmouseover = event => {
+    anim_projects()
+}
+document.querySelector(".projects_text").onclick = event => {
+    anim_projects()
+}
+
+// projects glowing stuff
+const handleOnMouseMove = e => {
+    const { currentTarget : target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+        
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+};
+
+for(const project of document.querySelectorAll(".project")) {
+    project.onmousemove = e => handleOnMouseMove(e)
+}
 
